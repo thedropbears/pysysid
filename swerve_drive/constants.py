2ddf141007ba82c77c50cb291e2c5d909c408f0f
Lucien Morey <lucienp@live.com.au>
2024-01-27 14:39:47 +1100

    add swerve drive motor sysid (#2)

---
+++ swerve_drive/constants.py
@@ -0,0 +1,25 @@
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import enum
+
+
+@enum.unique
+class TalonIds(enum.IntEnum):
+    drive_1 = 1
+    steer_1 = 5
+
+    drive_2 = 2
+    steer_2 = 6
+
+    drive_3 = 3
+    steer_3 = 7
+
+    drive_4 = 4
+    steer_4 = 8
+
+
+class OIConstants:
+    kDriverControllerPort = 0

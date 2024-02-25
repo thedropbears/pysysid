692f90100c7c39f1f796c0afa9991e9d0538c025
David Vo <auscompgeek@users.noreply.github.com>
2024-02-25 14:53:03 +1100

    flywheel: Accomodate follower motors

--- flywheel/subsystems/flywheel.py
+++ flywheel/subsystems/flywheel.py
@@ -11,30 +11,40 @@ from wpilib import sysid
 
 from phoenix6 import SignalLogger
 from phoenix6.configs import FeedbackConfigs, MotorOutputConfigs
-from phoenix6.controls import VoltageOut
+from phoenix6.controls import Follower, VoltageOut
 from phoenix6.signals import NeutralModeValue
 
 from wpimath.units import volts
 
+
+FollowerDescriptor = tuple[phoenix6.hardware.TalonFX, bool]
 
 
 class Flywheel(Subsystem):
     def __init__(
         self,
         flywheel_motor: phoenix6.hardware.TalonFX,
+        *followers: FollowerDescriptor,
         gearing: float,
         name: str | None = None,
     ) -> None:
         self.flywheel = flywheel_motor
+        self.followers = [motor for motor, _ in followers]
         if name is not None:
             self.setName(name)
 
-        flywheel_config = self.flywheel.configurator
         flywheel_motor_config = MotorOutputConfigs()
         flywheel_motor_config.neutral_mode = NeutralModeValue.COAST
         feedback_config = FeedbackConfigs().with_sensor_to_mechanism_ratio(gearing)
+
+        flywheel_config = self.flywheel.configurator
         flywheel_config.apply(flywheel_motor_config)
-        flywheel_config.apply(flywheel_gear_ratio_config)
+        flywheel_config.apply(feedback_config)
+
+        for motor, oppose_leader in followers:
+            motor.configurator.apply(flywheel_motor_config)
+            motor.configurator.apply(feedback_config)
+            motor.set_control(Follower(self.flywheel.device_id, oppose_leader))
 
         # Tell SysId to make generated commands require this subsystem, suffix test state in
         # WPILog with this subsystem's name ("drive")
@@ -53,11 +63,18 @@ class Flywheel(Subsystem):
     # characterized.
     def log(self, sys_id_routine: sysid.SysIdRoutineLog) -> None:
         (
-            sys_id_routine.motor("flywheel")
+            sys_id_routine.motor("leader")
             .voltage(self.flywheel.get_motor_voltage().value)
             .position(self.flywheel.get_position().value)
             .velocity(self.flywheel.get_velocity().value)
         )
+        for motor in self.followers:
+            (
+                sys_id_routine.motor(f"follower-{motor.device_id}")
+                .voltage(motor.get_motor_voltage().value)
+                .position(motor.get_position().value)
+                .velocity(motor.get_velocity().value)
+            )
 
     def recordState(self, state: sysid.State) -> None:
         if not self.logger_inited:
